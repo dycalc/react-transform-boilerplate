b1cc52139c3c406e0fed109e073ce55d6c6d5ad7
dycalc <dycalc@163.com>
2016-03-15 20:53:01 +0800

    material-ui

--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@ import { render } from 'react-dom';
 
 import App from './App';
 import './style/main.scss';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
 
 render(<App />, document.getElementById('root'));
